9b9dde892d4c57413b8c6ec17b43c490cf3f2a42
Julien <julien.millionbalard@lilo.org>
2025-12-21 16:56:09 +0100

    speed evolution

--- Assets/Code/GrabEvent.cs
+++ Assets/Code/GrabEvent.cs
@@ -1,10 +1,15 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using XCharts.Runtime;
+
 
 public class GrabEvent : MonoBehaviour
 {
+    [SerializeField] private Canvas _uiCanvas;
+    
     XRGrabInteractable _grab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +28,28 @@ public class GrabEvent : MonoBehaviour
     IEnumerator PlotDashboard()
     {
         yield return new WaitForSeconds(5f);
-        Position_Tracker.PrintLastPositions();
+
+        List<float> speeds = Position_Tracker.GetLastSpeeds();
+        
+        LineChart _lineChart = _uiCanvas.GetComponentInChildren<LineChart>(true);
+        _lineChart.GetChartComponent<YAxis>().axisLabel.formatter = "{value:F2}";
+
+
+        _lineChart.RemoveData();
+
+        if (_lineChart.series.Count == 0)
+        {
+            _lineChart.AddSerie<Line>("Speed");
+        }
+
+        for (int i = 0; i < speeds.Count; i++)
+        {
+            _lineChart.AddData(0, i, speeds[i]);
+        }
+
+        _lineChart.RefreshChart();
+        
+        _uiCanvas.gameObject.SetActive(true);
+        
     }
 }
--- Assets/Code/Position_Tracker.cs
+++ Assets/Code/Position_Tracker.cs
@@ -1,16 +1,14 @@
-using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.Rendering.UI;
 
 public class Position_Tracker : MonoBehaviour
 {
     [SerializeField] private float recordInterval = 1f; // seconds
-
+    
     private static readonly Queue<Vector3> LastPositions = new(10000);
     private float _timer;
+    
     
     private void Update()
     {
@@ -22,16 +20,10 @@ public class Position_Tracker : MonoBehaviour
         LastPositions.Enqueue(transform.position);
     }
 
-    private void OnApplicationQuit()
-    {
-        PrintLastPositions();
-    }
-
     public static void PrintLastPositions()
     {
         //TODO pretty print of graph + interface
         Debug.Log("Last recorded player positions:");
-
         var i = 1;
         foreach (var pos in LastPositions)
         {
@@ -39,6 +31,32 @@ public class Position_Tracker : MonoBehaviour
             i++;
         }
     }
+    public static List<float> GetLastSpeeds()
+    {
+        var speeds = new List<float>();
 
+        if (LastPositions.Count < 2)
+            return speeds;
 
+        Vector3 previous = Vector3.zero;
+        bool first = true;
+
+        foreach (var current in LastPositions)
+        {
+            if (first)
+            {
+                previous = current;
+                first = false;
+                continue;
+            }
+
+            float distance = Vector3.Distance(previous, current);
+            float speed = distance / 1; // recordInterval = 1
+
+            speeds.Add(speed);
+            previous = current;
+        }
+
+        return speeds;
+    }
 }
